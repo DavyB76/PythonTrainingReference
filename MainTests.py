d5de360435ae3bba9a0bc7d25dc21e3e1f8e40e9
davybureau <davy.bureau@gmail.com>
2018-06-19 13:39:04 +0200

    First test on Label/Reference on object

--- MainTests.py
+++ MainTests.py
@@ -1,4 +1,13 @@
 import unittest
 
+
+class TestVariableAreLabelsNotBoxes(unittest.TestCase):
+    def test_Should_Return_Same_List_When_Two_references_to_the_same_object_are_accessed(self):
+        a = [1, 2, 3]
+        b = a
+        a.append(4)
+        self.assertEqual(a is b, True)
+
+
 if __name__ == "__main__":
     unittest.main()
