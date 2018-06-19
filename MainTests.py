0b9f46472846a6863d097a63bde982cc760480cb
davybureau <davy.bureau@gmail.com>
2018-06-19 14:05:15 +0200

    new test on aliases and imposter sequence

--- MainTests.py
+++ MainTests.py
@@ -8,6 +8,21 @@ class TestVariableAreLabelsNotBoxes(unittest.TestCase):
         a.append(4)
         self.assertEqual(a is b, True)
 
+    def test_Should_Make_distinction_Between_Aliases_And_Imposters_When_aliases_and_different_objects_are_compared(self):
+        charles = {"name": "Charles L. Dodgson", "born": 1832}
+        lewis = charles
+
+        self.assertEqual(lewis is charles, True)
+        self.assertEqual(id(lewis), id(charles))
+
+        lewis["balance"] = 950
+
+        alex = {"name": "Charles L. Dodgson", "born": 1832, "balance": 950}
+
+        self.assertEqual(lewis["balance"], charles["balance"])
+
+        self.assertEqual(alex is not charles, True)
+        self.assertSequenceEqual(alex, charles)
 
 if __name__ == "__main__":
     unittest.main()
